45fab9cb5291633b7e29c846581d6c84b1e1dfaa
HalaDigitalLtd <ikramzaman@gmail.com>
2026-05-03 14:17:47 +0100

    Fix sitemap XML structure and include all pages

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,43 +1,21 @@
 export async function GET() {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-      <loc>https://haladigital.co.uk</loc>
-    </url>
-    <url>
-      <loc>https://haladigital.co.uk/vat-sic</loc>
-    </url>
-    <url>
-      <loc>https://haladigital.co.uk/vat-registration</loc>
-    </url>
-    <url>
-      <loc>https://haladigital.co.uk/vat-property</loc>
-    </url>
-    <url>
-      <loc>https://haladigital.co.uk/quote</loc>
-      <url>
-  <loc>https://haladigital.co.uk/website-design-for-small-business-uk</loc>
-</url>
-<url>
-  <loc>https://haladigital.co.uk/small-business-website-cost-uk</loc>
-</url>
-<url>
-  <loc>https://haladigital.co.uk/accountant-website-design-uk</loc>
-</url>
-<url>
-  <loc>https://haladigital.co.uk/self-assessment-calculator</loc>
-</url>
-<url>
-  <loc>https://haladigital.co.uk/dividend-tax-calculator</loc>
-</url>
-<url>
-  <loc>https://haladigital.co.uk/salary-calculator</loc>
-</url>
-<url>
-  <loc>https://haladigital.co.uk/company-formation</loc>
-</url>
-    </url>
-  </urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url><loc>https://haladigital.co.uk</loc></url>
+  <url><loc>https://haladigital.co.uk/hala</loc></url>
+  <url><loc>https://haladigital.co.uk/website-design-for-small-business-uk</loc></url>
+  <url><loc>https://haladigital.co.uk/small-business-website-cost-uk</loc></url>
+  <url><loc>https://haladigital.co.uk/accountant-website-design-uk</loc></url>
+  <url><loc>https://haladigital.co.uk/company-formation</loc></url>
+  <url><loc>https://haladigital.co.uk/quote</loc></url>
+  <url><loc>https://haladigital.co.uk/self-assessment-calculator</loc></url>
+  <url><loc>https://haladigital.co.uk/dividend-tax-calculator</loc></url>
+  <url><loc>https://haladigital.co.uk/salary-calculator</loc></url>
+  <url><loc>https://haladigital.co.uk/how-to-start-a-limited-company-uk</loc></url>
+  <url><loc>https://haladigital.co.uk/vat-sic</loc></url>
+  <url><loc>https://haladigital.co.uk/vat-registration</loc></url>
+  <url><loc>https://haladigital.co.uk/vat-property</loc></url>
+</urlset>`;
 
   return new Response(xml, {
     headers: {
